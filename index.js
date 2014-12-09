346c365ecb0df74181dfc7a0ea84d02da9fbfb2e
Patrick Gunderson <patrick@toolofna.com>
2014-12-09 15:50:35 -0800

    added better namspacing

--- index.js
+++ index.js
@@ -17,48 +17,52 @@ var colors = require("colors");
 
 
 function pluginError (message) {
-  return new PluginError('gulp-jade-jst-concat', message)
+  return new PluginError('gulp-jade-jst-concat', message);
 }
 
 
 module.exports = function processFiles(fileName, _opts) {
-  if (!fileName) throw pluginError('Missing fileName')
+  if (!fileName) throw pluginError('Missing fileName');
+
+  console.log(this);
 
 
-  var defaults = {}
-    , opts = _.extend({}, defaults, _opts)
-    , data = {}
+  var defaults = {
+      basepath: path.dirname(fileName)
+    },
+    opts = _.extend({}, defaults, _opts),
+    data = {};
 
   function write (file) {
-    if (file.isNull()) return
-    if (file.isStream()) return this.emit('error', pluginError('Streaming not supported'))
+    if (file.isNull()) return;
+    if (file.isStream()) return this.emit('error', pluginError('Streaming not supported'));
     
       //do stuff
 
 
-    var basename = path.basename(file.path, ".js");
+    var basename = path.relative(opts.basepath, file.path).split(".js")[0];
     // Change the function name to the basename for the template
     file.contents = new Buffer(file.contents.toString().replace("function template", "\"" + basename + "\": function"));
-		
+    
     data[basename] = file.contents;
   }
 
   function end () {
     var concatedData = "var jade = require('jade/runtime'); module.exports = {\n";
-    var templateList = []
+    var templateList = [];
     _.each(data, function(fn, name){
       concatedData += fn.toString() + ",\n";
-    })
+    });
     concatedData += "}";
 
 
     this.queue(new File({
       path: fileName,
       contents: new Buffer(concatedData)
-    }))
+    }));
 
-    this.queue(null)
+    this.queue(null);
   }
 
-  return through(write, end)
-}
+  return through(write, end);
+};
